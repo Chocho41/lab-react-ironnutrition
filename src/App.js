8f36118190bc47e83416ac1c8f3435d8e5806d8e
Stephane Mbesse <stephane.mbesse@gmail.com>
2021-05-26 23:43:21 +0200

    iteration 3 Done merci Melisa

--- src/App.js
+++ src/App.js
@@ -3,48 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
-import FoodBox from './components/FoodBox'
-import Form from './components/Form'
-
+import FoodBox from './components/FoodBox';
+import Form from './components/Form';
 
 export class App extends React.Component {
-    state = {
-        foods: foods,
-        isClick : false,
-        name: "",
-        numberOfCalories: "",
-        image: "",
-    };
+  state = {
+    foods: foods,
+    isClick: false,
+    name: '',
+    numberOfCalories: '',
+    image: '',
+    newFood:{},
+  };
 
-    handleChange = (event) => {
-      console.log(event.target.value);
-      this.setState({
-          [event.target.name]: event.target.value,
-      })
-  }
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleSubmit = (event) => {
-      console.log(event.target);
-      event.preventDefault();
-      console.log("I have been clicked!");
-      console.log(this.state);
+  handleSubmit = (event) => {
+    console.log(event.target);
+    event.preventDefault();
+    console.log('I have been clicked!');
+    console.log(this.state);
+    console.log(this.state.foods);
+    this.setState({
+      foods: [...this.state.foods, {
+        name: this.state.name,
+        calories: this.state.numberOfCalories,
+        image: this.state.image,
+        quantity: 1 }],
+      isClick: false,
+    });
+    console.log(foods)
+  };
 
-  }
-  
   handleClickForm = () => {
-    this.setState({isClick : true});
-  }
-  
+    this.setState({ isClick: true });
+  };
+
   render() {
     return (
       <div className="App">
-          {this.state.foods.map((food) =><FoodBox callBackFn={this.handleClickForm} food={food}/>)}
-        {this.state.isClick && <Form callbackSubFn={this.handleSubmit} callbackOnChgFn={this.handleChange}/>}
+        {this.state.foods.map((food) => {
+         return <FoodBox callBackFn={this.handleClickForm} food={food} />
+})}
+        {this.state.isClick && (
+          <Form
+            callbackSubFn={this.handleSubmit}
+            callbackOnChgFn={this.handleChange}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
-
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,5 +1,5 @@
 import 'bulma/css/bulma.css';
-import foods from '../foods.json';
+// import foods from '../foods.json';
 
 import React from 'react'
 
@@ -10,40 +10,40 @@ const FoodBox = (props) => {
         props.callBackFn();
     }
 
-    console.log(props);
+    // console.log(props);
     return (
-        <div>
-            <div className="box">
-                <article className="media">
-                    <div className="media-left">
-                        <figure className="image is-64x64">
-                            <img src={props.food.image} />
-                        </figure>
-                    </div>
-                    <div className="media-content">
-                        <div className="content">
-                            <p>
-                                <strong>{props.food.name}</strong> <br />
-                                <small>{props.food.calories} cal</small>
-                            </p>
-                        </div>
-                    </div>
-                    <div className="media-right">
-                        <div className="field has-addons">
-                            <div className="control">
-                                <input className="input" type="number" value="1" />
-                            </div>
-                            <div className="control">
-                                <button onClick={handClick} className="button is-info">
-                                    +
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </article>
+      <div>
+        <div className="box">
+          <article className="media">
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img src={props.food.image} alt={props.food.name} />
+              </figure>
             </div>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>{props.food.name}</strong> <br />
+                  <small>{props.food.calories} cal</small>
+                </p>
+              </div>
+            </div>
+            <div className="media-right">
+              <div className="field has-addons">
+                <div className="control">
+                  <input className="input" type="number" value="1" />
+                </div>
+                <div className="control">
+                  <button onClick={handClick} className="button is-info">
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+          </article>
         </div>
-    )
+      </div>
+    );
 }
 
 export default FoodBox
